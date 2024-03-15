c25fc35f1d4e54ab910c7e1363140e02c0d4c662
davronkhandamov <xamdamovdavron6@gmail.com>
2024-03-15 11:44:58 +0500

    added dot env

--- app/db.py
+++ app/db.py
@@ -1,8 +1,12 @@
+import os
+
+import dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-database_url = "postgresql://nxhimsrt:Erbd22xs5GKHa4vDXmB9Uf_5MV4bOXz3@bubble.db.elephantsql.com/nxhimsrt"
+dotenv.load_dotenv()
+database_url = os.getenv("DATABASE_URL")
 
 engine = create_engine(url=database_url)
 session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
